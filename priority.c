a0c5171295973a60ac15eeb440d72d221f82edb0
bshivani166 <bshivani166@gmail.com>
2019-04-01 03:13:11 -0700

    Added priority creating

---
+++ priority.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main(){
+    int students[10],gifts[10],i;
+    for(i=0;i<10;i++)
+    {
+        printf("Enter Number Of Gifts For Student %d :",i+1);
+        scanf("%d",&gifts[i]);
+    }
+    printf("\n\nStudent\tGifts\n");
+    for(i=0;i<10;i++)
+    {
+        printf("%d\t%d\n",i+1,gifts[i]);
+    }
+}
