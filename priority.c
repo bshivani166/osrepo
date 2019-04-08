44793c0e29c543a071409f960844be2c045e0762
bshivani166 <bshivani166@gmail.com>
2019-04-08 03:32:31 -0700

    added priority listing

--- priority.c
+++ priority.c
@@ -1,21 +1,35 @@
 #include<stdio.h>
 
-void sort()
-{
-
-  //dummy sort funtion
-}
 
 void main(){
-    int students[10],gifts[10],i;
-    for(i=0;i<10;i++)
-    {
-        printf("Enter Number Of Gifts For Student %d :",i+1);
-        scanf("%d",&gifts[i]);
+    int students[10][2],i,j,tempStd,tempGifts;
+    
+    for (i=0;i<10;i++){
+        printf("Enter Number Of Gifts For Student %d: ",i+1);
+        students[i][0] = i+1;
+        scanf("%d",&students[i][1]);
     }
-    printf("\n\nStudent\tGifts\n");
-    for(i=0;i<10;i++)
-    {
-        printf("%d\t%d\n",i+1,gifts[i]);
-    }
+
+     for(i=0;i<10;i++)
+     {
+      for(j=i+1;j<10;j++)
+      {
+         if(students[i][1] < students[j][1])
+         {
+            tempGifts = students[i][1];
+            tempStd = students[i][0];
+
+            students[i][0] = students[j][0];
+            students[i][1] = students[j][1];
+            
+            students[j][1]=tempGifts;
+            students[j][0]=tempStd;
+         }
+      }
+   }
+    printf("Student Gifts\n");
+    for (i=0;i<10;i++){
+        printf("%d\t %d\n",students[i][0],students[i][1]);
+    }    
+
 }
